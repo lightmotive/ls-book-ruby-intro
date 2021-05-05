a62f4edb4bff19494eebdfd2fc8e5ebb67edb057
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-05 21:07:08 +0000

    Rename files to match exercise numbers

---
+++ the_basics/exercise_2.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: false
+
+require_relative 'number_places'
+
+number_places = NumberPlaces.new
+
+puts number_places.get_hash(9783)
+puts number_places.get_hash(9783, 5)
+puts number_places.get_hash(9783, 3)
+# This would raise a standard error:
+# puts number_places.get_hash(9783, 0)
--- the_basics/number_places.rb
+++ the_basics/number_places.rb
@@ -1,35 +1,35 @@
 # frozen_string_literal: false
 
-def place_to_num(place, base = 10)
-  place > 1 ? base**(place - 1) : 1
-end
+class NumberPlaces
+  def get_hash(num, places = nil)
+    num = num.to_i
+    places = places.nil? || num.to_s.length < places ? num.to_s.length : places
+    raise StandardError, 'places should be greater than 0' unless places.positive?
 
-# Mod until reaching place number, then divide
-def num_to_places(num, place)
-  length = num.to_s.length
+    places_hash = {}
+    places.downto(1) do |place|
+      places_hash[:"#{place_to_num(place)}s"] = num_to_places(num, place)
+    end
 
-  length.downto(place) do |p|
-    place_num = place_to_num(p)
-    break num / place_num if p == place
-
-    num = num % place_num
-  end
-end
-
-def number_places(num, places = nil)
-  num = num.to_i
-  places = places.nil? || num.to_s.length < places ? num.to_s.length : places
-  raise StandardError, 'places should be greater than 0' unless places.positive?
-
-  places_hash = {}
-  places.downto(1) do |place|
-    places_hash[:"#{place_to_num(place)}s"] = num_to_places(num, place)
+    places_hash
   end
 
-  places_hash
-end
+  private
 
-puts number_places(9783)
-puts number_places(9783, 5)
-puts number_places(9783, 3)
-puts number_places(9783, 0)
+  # Convert a place to a number, e.g., place 4 (Thousands) with default base 10 would return 1000
+  def place_to_num(place, base = 10)
+    place > 1 ? base**(place - 1) : 1
+  end
+
+  # Mod until reaching place number, then divide
+  def num_to_places(num, place)
+    length = num.to_s.length
+
+    length.downto(place) do |p|
+      place_num = place_to_num(p)
+      break num / place_num if p == place
+
+      num = num % place_num
+    end
+  end
+end
