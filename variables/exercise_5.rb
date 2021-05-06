a3b3a071bfc447f02c64a45c12472af71f7e823a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-06 17:10:38 +0000

    Complete Variables exercise 5
    - Scope review.

---
+++ variables/exercise_5.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: false
+
+# Scope review...
+
+x = 0
+3.times do
+  x += 1
+end
+puts x
+
+# => 3
+# The variable x is declared in the outer scope, so it's available in the inner times method block scope.
+
+# *** Imagine a separate file/program after this line for the purposes of this scope review...
+
+y = 0
+3.times do
+  y += 1
+  x = y
+end
+puts x
+
+# The program will increment the value of y to 3.
+# Then, it will raise an error because x was declared in the inner scope of the times method block scope.
+#   Therefore, x is not available outside of it.
