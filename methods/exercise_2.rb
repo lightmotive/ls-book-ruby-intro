42207c10981447d2d83c37b3eca40e3d8b6274a1
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-07 23:46:14 +0000

    Complete Methods exercise 2

---
+++ methods/exercise_2.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+x = 2
+# => 2
+
+puts x = 2
+# => nil
+
+p name = 'Joe'
+# => "Joe"
+# Gotcha: p, unlike puts or print, returns the value of what is printed.
+
+four = 'four'
+# => "four"
+
+print something = 'nothing'
+# => nil
