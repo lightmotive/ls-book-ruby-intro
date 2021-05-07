ca756ccdf202dfda83582d8206d8e7db4cf4f961
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-07 14:23:51 +0000

    Methods chapter practice
    - Add say.rb and mutate.rb

---
+++ methods/mutate.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+a = [1, 2, 3]
+
+# Example of a method definition that modifies its argument permanently
+def mutate(array)
+  array.pop
+end
+
+p "Before mutate method: #{a}"
+mutate(a)
+p "After mutate method: #{a}"
