1410cf7867cd0d02d9a77b3310e835da7c28991b
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-07 23:57:39 +0000

    Complete Methods exercise 4

--- methods/exercise_4.rb
+++ methods/exercise_4.rb
@@ -1,1 +1,11 @@
 # frozen_string_literal: true
+
+def scream(words)
+  words += '!!!!'
+  return
+  puts words
+end
+
+scream('Yippeee')
+# nothing put to screen because return statement preceeds puts words
+# => "Yippeee!!!!"
