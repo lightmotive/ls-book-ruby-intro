e6a26dc4f8eb781691d618808ffae2b3109af351
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-08 00:09:52 +0000

    Complete Methods exercise 6

--- methods/exercise_6.rb
+++ methods/exercise_6.rb
@@ -1,1 +1,10 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
+
+# What does the following error message indicate?
+#
+# ArgumentError: wrong number of arguments (1 for 2)
+#   from (irb):1:in `calculate_product'
+#   from (irb):4
+#   from /Users/username/.rvm/rubies/ruby-2.5.3/bin/irb:12:in `<main>'
+#
+# It indicates that the wrong number of arguments was passed to calculate_product (1 passed, expected 2).
