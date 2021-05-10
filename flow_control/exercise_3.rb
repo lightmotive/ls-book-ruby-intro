e3264d2975fb65ee6197d84d3333572403c1d957
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-10 16:34:09 +0000

    Complete Flow Control exercise 3

---
+++ flow_control/exercise_3.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: false
+
+print 'Please enter a positive number: '
+input_number = gets.chomp.to_i
+
+if input_number.negative?
+  puts 'You entered a negative number, which is not allowed here.'
+elsif input_number <= 50
+  puts 'You entered a number inclusively between 0 and 50'
+elsif input_number > 50 && input_number <= 100
+  puts 'You entered a number inclusively between 51 and 100'
+elsif input_number > 100
+  puts 'You entered a number greater than 100'
+end
