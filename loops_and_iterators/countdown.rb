339d134a2062f8ed2f290ba4775b4ebd5590e0bc
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-10 19:17:29 +0000

    Practice Loops & Iterators: until loop
    - Replace while with until

--- loops_and_iterators/countdown.rb
+++ loops_and_iterators/countdown.rb
@@ -3,7 +3,9 @@
 print 'Enter a number from which to count down to zero: '
 x = gets.chomp.to_i
 
-while x >= 0
+# while x >= 0
+# Rewrite while above to use until
+until x.negative?
   puts x
   x -= 1
 end
