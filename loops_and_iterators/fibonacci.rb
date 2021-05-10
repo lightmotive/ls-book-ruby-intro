8b7fff449a80687c2c29faf80268e6033fe6a532
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-10 22:56:12 +0000

    Extend fibonacci to support negative numbers

--- loops_and_iterators/fibonacci.rb
+++ loops_and_iterators/fibonacci.rb
@@ -1,14 +1,30 @@
-# frozen_string_literal: false
+# frozen_string_literal: true
 
-def fibonacci(sequence)
-  if sequence < 2
+def fibonacci_positive(sequence)
+  raise StandardError, 'Fibonacci sequence number must be >= 0' unless sequence >= 0
+
+  if sequence >= 0 && sequence < 2
     sequence
   else
-    fibonacci(sequence - 1) + fibonacci(sequence - 2)
+    fibonacci_positive(sequence - 1) + fibonacci_positive(sequence - 2)
   end
 end
 
+def fibonacci(sequence)
+  sequence_original = sequence
+
+  sequence *= -1 if sequence.negative?
+  fibonacci_n = fibonacci_positive(sequence)
+
+  return ((-1)**(sequence + 1)) * fibonacci_n if sequence_original.negative?
+
+  fibonacci_n
+end
+
+puts "-8: #{fibonacci(-8)}"
+puts "-5: #{fibonacci(-5)}"
+puts "-2: #{fibonacci(-2)}"
 puts "0: #{fibonacci(0)}"
-puts "3: #{fibonacci(3)}"
-puts "6: #{fibonacci(6)}"
-puts "9: #{fibonacci(9)}"
+puts "2: #{fibonacci(2)}"
+puts "5: #{fibonacci(5)}"
+puts "8: #{fibonacci(8)}"
