8c6360bde79ca858c967d942000f27b21514f128
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-10 16:14:12 +0000

    Complete Flow Control exercise 1

---
+++ flow_control/exercise_1.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Evaluate the following
+# (32 * 4) >= 129 # false, because 128 is not greater than or equal to 129.
+# false != !true # false, because !true is false, and it's false to write false does not equal false.
+# true == 4 # false, because different value types cannot be equal.
+# false == (847 == '874') # true, because false == false.
+# (!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false # true, because there's at least one true statement
+#   amongst several or statements.
