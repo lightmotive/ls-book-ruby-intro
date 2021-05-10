f2c21a01e7164cdc8f0648b3523d0c877941728d
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-10 21:41:26 +0000

    Add counter to printed names list

--- loops_and_iterators/practice_each.rb
+++ loops_and_iterators/practice_each.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 names = %w[Bob Joe Steve Janice Susan Helen]
+list_number = 1
 
-names.each { |name| puts name }
+names.each do |name|
+  puts "#{list_number}. #{name}"
+  list_number += 1
+end
