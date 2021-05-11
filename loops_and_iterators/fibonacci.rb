7d8c4c83c0238e6a20bc9f6805d82f659b6b0af4
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-11 22:33:22 +0000

    Refactor fibonacci.rb for clarity

--- loops_and_iterators/fibonacci.rb
+++ loops_and_iterators/fibonacci.rb
@@ -10,21 +10,26 @@ def fibonacci_positive(sequence)
   end
 end
 
+# Sequence number can be positive or negative
 def fibonacci(sequence)
-  sequence_original = sequence
+  sequence_positive = sequence.negative? ? sequence * -1 : sequence
+  fibonacci_positive = fibonacci_positive(sequence_positive)
 
-  sequence *= -1 if sequence.negative?
-  fibonacci_n = fibonacci_positive(sequence)
+  return ((-1)**(sequence_positive + 1)) * fibonacci_positive if sequence.negative?
 
-  return ((-1)**(sequence + 1)) * fibonacci_n if sequence_original.negative?
-
-  fibonacci_n
+  fibonacci_positive
 end
 
 puts "-8: #{fibonacci(-8)}"
 puts "-5: #{fibonacci(-5)}"
+puts "-4: #{fibonacci(-4)}"
+puts "-3: #{fibonacci(-3)}"
 puts "-2: #{fibonacci(-2)}"
+puts "-1: #{fibonacci(-1)}"
 puts "0: #{fibonacci(0)}"
+puts "1: #{fibonacci(1)}"
 puts "2: #{fibonacci(2)}"
+puts "3: #{fibonacci(3)}"
+puts "4: #{fibonacci(4)}"
 puts "5: #{fibonacci(5)}"
 puts "8: #{fibonacci(8)}"
