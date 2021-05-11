6e5f617497b89d15d89cd3b045c1261931c25caf
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-11 23:13:27 +0000

    Complete Loops & Iterators exercise 3

---
+++ loops_and_iterators/exercise3.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: false
+
+def count_to_zero(from_number)
+  return unless from_number >= 0
+
+  puts(from_number.positive? ? from_number : '0 => Go!')
+  count_to_zero(from_number - 1) if from_number.positive?
+end
+
+count_to_zero(10)
