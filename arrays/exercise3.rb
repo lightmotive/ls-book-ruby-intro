b024906274821766633134cf5a0101424206364a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-14 19:47:14 +0000

    Complete Arrays exercise 3

---
+++ arrays/exercise3.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# How do you return the word "example" from the following array?
+arr = [%w[test hello world], %w[example mem]]
+
+puts arr.last.first
+puts arr[1][0]
