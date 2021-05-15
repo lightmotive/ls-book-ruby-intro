f8a1a1851f3b51be3dcb63cd30984ed6928a9a41
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-15 20:50:10 +0000

    Refactor Hashes exercise 2
    - Rename hash objects to reflect an OOP scenario

--- hashes/exercise2.rb
+++ hashes/exercise2.rb
@@ -3,15 +3,15 @@
 # 2. Look at Ruby's merge method. Notice that it has two versions. What is the difference between merge and merge!?
 #    Write a program that uses both and illustrate the differences.
 
-chocolate_overview = { type: 'Very Dark' }
-chocolate_detail = { cacao_percentage: 80 }
+candy = { type: 'Very Dark Chocolate' }
+chocolate_candy = { cacao_percentage: 80 }
 
-chocolate = chocolate_overview.merge(chocolate_detail)
-puts 'Merged into chocolate:'
-p chocolate
-puts 'Notice that chocolate_overview has not been modified:'
-p chocolate_overview
+product_details = candy.merge(chocolate_candy)
+puts 'Merged into product_details:'
+p product_details
+puts 'Notice that candy has not been modified:'
+p candy
 
-chocolate_overview.merge!(chocolate_detail)
-puts '...merged with merge!, which modifies chocolate_overview:'
-p chocolate_overview
+candy.merge!(chocolate_candy)
+puts '...merged with merge!, which modifies candy:'
+p candy
