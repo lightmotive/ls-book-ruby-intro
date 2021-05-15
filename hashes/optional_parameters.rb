a9ba0c57118465e0967a055e123ae3c633db36a4
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-15 19:26:24 +0000

    Practice Hashes: optional parameters
    - Demonstrate optional curly braces

--- hashes/optional_parameters.rb
+++ hashes/optional_parameters.rb
@@ -4,10 +4,11 @@ def greeting(name, options = {})
   if options.empty?
     puts "Hi, my name is #{name}"
   else
-    puts "Hi, my name is #{name} and I'm #{options[:age]}" \
-         " years old and I live in #{options[:city]}."
+    puts "Hi, my name is #{name}. I'm #{options[:age]}" \
+         " years old and live in #{options[:city]}."
   end
 end
 
 greeting('Bob')
 greeting('Bob', { age: 62, city: 'New York City' })
+greeting('Jane', age: 41, city: 'Chicago')
