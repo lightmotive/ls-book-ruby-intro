9db8fb11130169787509357c896dbdc16fb464d2
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-18 21:51:36 +0000

    Practice More Stuff: Procs
    - Apply proc.call shorthand

--- more_stuff/passing_proc.rb
+++ more_stuff/passing_proc.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 def take_proc(proc)
-  [1, 2, 3, 4, 5].each do |number|
-    proc.call number
-  end
+  [1, 2, 3, 4, 5].each(&proc)
+  # (&proc) is shorthand for:
+  #   do |number|
+  #     proc.call number
+  #   end
+  # That works well when the proc parameters match the calling method's parameters.
 end
 
 proc = proc do |number|
