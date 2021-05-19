45dcabdcd03fab1fc9fa8182d37fd5a4c1a28558
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-19 04:32:09 +0000

    Practice More Stuff: Exception Handling
    - Add to exception_example.rb

--- more_stuff/exception_example.rb
+++ more_stuff/exception_example.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
-begin
-  # perform some dangerous operation
-rescue StandardError
-  # do this if operation fails
-  # for example, log the error
+names = ['bob', 'joe', 'steve', nil, 'frank']
+
+names.each do |name|
+  begin
+    puts "#{name}'s name has #{name.length} letters in it."
+  rescue
+    puts "Something went wrong!"
+  end
 end
