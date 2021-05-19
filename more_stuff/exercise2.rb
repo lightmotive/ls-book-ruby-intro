fbdccce7a870c1507190a926de09dd1405b7d1fe
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-19 15:11:10 +0000

    Play with More Stuff exercise 2

--- more_stuff/exercise2.rb
+++ more_stuff/exercise2.rb
@@ -2,10 +2,25 @@
 
 # What will the following program print to the screen? What will it return?
 
-def execute(&block)
-  block
+def execute_block(&block)
+  block.call
 end
 
-execute { puts 'Hello from inside the execute method!' }
-# That will do nothing because block is not called in the execute method (no block.call)
-# It returns a Proc.
+def execute_proc(proc)
+  proc.call
+end
+
+def chained_block(&block)
+  block.call
+  proc { puts 'Hello from block 2' }
+end
+
+p(execute_block { puts 'Hello from whatever calls this block!' })
+
+hello = proc { puts 'Hello from whatever calls "hello" :-)' }
+p(execute_proc(hello))
+
+(chained_block { puts 'Hello from Block 1' }).call
+
+# Learned: block.call and proc.call both return nil.
+#   One can call a block argument and return a proc to call after.
