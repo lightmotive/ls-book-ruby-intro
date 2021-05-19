50df3307173eb29e1b1ebefa52fda72ae67d38c3
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-19 16:40:22 +0000

    Complete book exercise 2

--- exercises/exercise1.rb
+++ exercises/exercise1.rb
@@ -5,3 +5,10 @@
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 arr.each { |e| puts e }
+
+# Exercise 2: Same as above, but only print out values greater than 5.
+puts '# Exercise 2'
+arr.each { |e| puts e if e > 5 }
+# With SRP in mind, one might want to separate filtering from action, e.g.:
+puts '# Exercise 2.alt-1:'
+arr.select { |e| e > 5 }.each { |e| puts e }
