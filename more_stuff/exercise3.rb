c799b52d8479aa67ff8e5822eb4bd73223ab902b
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-19 16:20:09 +0000

    Complete More Stuff exercise 3

---
+++ more_stuff/exercise3.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Q. What is exception handling and what problem does it solve?
+# A. Exception handling is a structure, usually a built-in language feature, programmers use to handle
+#    (change the control flow of) possible errors so programs can gracefully respond to problems like
+#    unexpected input or results rather than exiting the program entirely.
+#
+#    Programmers usually handle specific exception types because they don't want the same handler to manage
+#    every exception type.
