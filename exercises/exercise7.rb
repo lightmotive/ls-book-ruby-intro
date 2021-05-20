4fed5deb4687dba7c883817eb47257637d2ea556
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-20 20:57:36 +0000

    Complete book exercise 7

---
+++ exercises/exercise7.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Q. What's the major difference between an Array and a Hash?
+
+# A. Arrays are organized by ordered index; hashes are organized by key, which is an associative data structure.
+#    Before Ruby v1.9, hashes were not ordered (programmers could not expect a specific order when iterating).
+#      Starting with v1.9, hash items (key-value pairs) have a consistent order.
