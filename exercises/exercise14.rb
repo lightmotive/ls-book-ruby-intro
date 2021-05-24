3ffa8669eeccd227f290c2158159bbcda1ca6801
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-05-24 15:40:09 +0000

    Complete book exercise 14

---
+++ exercises/exercise14.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Take the following array:
+
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+
+# ...and turn it into a new array that consists of strings containing one word.
+# (ex. ["white snow", etc...] -> ["white", "snow", etc...].
+
+a_words = a.map(&:split).flatten!
+p a_words
